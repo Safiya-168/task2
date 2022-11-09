9d2cbce884f29d520fbde7645190c2c62920afc6
Safiya-168 <98414048+Safiya-168@users.noreply.github.com>
2022-11-09 16:12:00 +0530

    Add files via upload

---
+++ 3rd.py
@@ -0,0 +1,12 @@
+'''odulus Ten
+PrepBuddy gave you a number 
+x
+, and asked you to find the remainder of 
+x
+ divided by 
+10
+.'''
+a=int(input("enter the value="))
+b=10
+c=a%b
+print("the remainder is",c)
