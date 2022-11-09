7c936b34971183e0bcafd4281e5847600655a347
Safiya-168 <98414048+Safiya-168@users.noreply.github.com>
2022-11-09 15:53:38 +0530

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,11 @@
+#Multiply Numbers
+#Let's practice multiplication a bit.
+#Write a program to take two integers 
+#x
+ #and 
+#y
+ #as input from the user, multiply those integers and print the product.'''
+a=int(input("enter the no"))
+b=int(input("enter the no"))
+c=a*b
+print("the product is",c)
